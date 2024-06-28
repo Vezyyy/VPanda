885af2ca5f3a29a0de3f8ba85bf2ccd19c256645
Vezyy <108421377+Vezyyy@users.noreply.github.com>
2024-06-28 12:19:01 +0200

    JavaScript Update
    vezyy 28.06.2024 | 12:18

--- Panda.js
+++ Panda.js
@@ -1,1 +1,30 @@
+// LOADING SCREEN 
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.loading-screen');
+    loader.classList.add('fade-out');
 
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 1000);
+});
+
+// Scroll to top function
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Check if cookies are already accepted
+window.addEventListener('DOMContentLoaded', (event) => {
+    if (!localStorage.getItem('cookiesAccepted')) {
+        document.getElementById('cookie-notification').style.display = 'block';
+    }
+
+    // Accept cookies function
+    document.getElementById('accept-cookies').addEventListener('click', function () {
+        localStorage.setItem('cookiesAccepted', 'true');
+        document.getElementById('cookie-notification').style.display = 'none';
+    });
+});
